af9057882f03fa41ff5e4522a8f935c6c620a246
Kapiira <hannes.tagerud@gmail.com>
2020-04-20 09:18:46 +0100

    Added so you can see you are logged in as

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from '@reach/router';
+import { UserContext } from '../store/user';
 
 function Header() {
+  const user = useContext(UserContext);
   return (
     <header>
       <Link to="/">NC News</Link>
+
+      <div className="loggedIn">
+        Logged in as: <strong>{user}</strong>
+      </div>
     </header>
   );
 }
